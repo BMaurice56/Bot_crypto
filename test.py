e0036addb44ee6090062a92691fa0649bbb89e32
Benoît <benoitmaurice56@protonmail.com>
2022-08-06 23:19:45 +0200

    Test

--- test.py
+++ test.py
@@ -1,1 +1,19 @@
 from main import *
+from datetime import datetime
+from zoneinfo import ZoneInfo
+import locale
+
+locale.setlocale(locale.LC_ALL, 'fr_FR.UTF-8')
+
+etat = etat_bot("lecture").split(";")
+
+# Conversion de l'ancienne date sauvegarder et de la date actuelle en seconde
+ancienne_date = datetime.strptime(etat[0], "%A %d %B %Y %H:%M:%S")
+
+ancienne_date = int(ancienne_date.strftime("%s"))
+
+date = int(datetime.now(tz=ZoneInfo("Europe/Paris")).strftime("%s"))
+
+print(date, ancienne_date)
+
+print(int(time.time()))
