5745c7697aa6207c22d04de12551c3e3ea4a7fca
Benoit <benoitmaurice56@protonmail.com>
2023-03-17 23:56:04 +0100

    Passage fonction a code corps principale

--- recuperation_fichier_erreur.py
+++ recuperation_fichier_erreur.py
@@ -1,39 +1,31 @@
 import paramiko
 
+# Créer une instance de la classe Paramiko SSHClient
+ssh = paramiko.SSHClient()
 
-def récupération_fichier():
-    """
-    Récupère les fichiers logs sur le serveur
-    """
-    # Créer une instance de la classe Paramiko SSHClient
-    ssh = paramiko.SSHClient()
+# Autorise les connexions SSH à des hôtes non connus
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-    # Autorise les connexions SSH à des hôtes non connus
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+# Connection au serveur SSH
+ssh.connect('212.227.209.205', username='root', password='I50ja%1ItV')
 
-    # Connection au serveur SSH
-    ssh.connect('212.227.209.205', username='root', password='I50ja%1ItV')
+# Démarre la session SFTP
+sftp = ssh.open_sftp()
 
-    # Démarre la session SFTP
-    sftp = ssh.open_sftp()
+# Télécharge les fichiers depuis le serveur SSH
+sftp.get('/home/Bot_crypto/fichier_log/log_erreur.txt',
+         "log_erreur.txt")
 
-    # Télécharge les fichiers depuis le serveur SSH
-    sftp.get('/home/Bot_crypto/fichier_log/log_erreur.txt',
-             "log_erreur.txt")
+sftp.get('/home/Bot_crypto/fichier_log/log_recap.txt',
+         "log_recap.txt")
 
-    sftp.get('/home/Bot_crypto/fichier_log/log_recap.txt',
-             "log_recap.txt")
+ssh.exec_command(
+    'echo > /home/Bot_crypto/fichier_log/log_erreur.txt')
+ssh.exec_command(
+    'echo > /home/Bot_crypto/fichier_log/log_recap.txt')
 
-    ssh.exec_command(
-        'echo > /home/Bot_crypto/fichier_log/log_erreur.txt')
-    ssh.exec_command(
-        'echo > /home/Bot_crypto/fichier_log/log_recap.txt')
+# Ferme la session SFTP
+sftp.close()
 
-    # Ferme la session SFTP
-    sftp.close()
-
-    # Déconnection du serveur SSH
-    ssh.close()
-
-
-récupération_fichier()
+# Déconnection du serveur SSH
+ssh.close()
