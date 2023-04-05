8704eeb2edbb33f1db32100a45fe83f4e2903d95
Benoit <benoitmaurice56@protonmail.com>
2023-04-05 21:45:56 +0200

    Modification temps attente multiple démarrage

--- bot_discord.py
+++ bot_discord.py
@@ -145,7 +145,7 @@ class Bot_Discord(commands.Bot):
                             if f"{symbol}_started" in self.kucoin.dico_partage:
                                 del self.kucoin.dico_partage[f"{symbol}_started"]
 
-                                await asyncio.sleep(3)
+                                await asyncio.sleep(2)
 
                                 break
 
